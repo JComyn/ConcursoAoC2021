6191aedadf77423247ce0b41205aaec06c96590b
JComyn <64434554+JComyn@users.noreply.github.com>
2021-12-04 17:51:38 +0100

    Update main.py

--- D01/main.py
+++ D01/main.py
@@ -1,1 +1,13 @@
+#Open and read the input file
+with open("input.txt") as f:
+    lines = f.readlines()
 
+#Convert it to a list of ints
+lines = list(map(int, lines))
+
+count=0
+for i in range(1, len(lines)):
+    if lines[i-1] < lines[i]:
+        count+=1
+
+print(count)
