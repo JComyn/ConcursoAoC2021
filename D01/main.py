eaae42a1dbe9729689bb1c4342cec9909b188605
JComyn <64434554+JComyn@users.noreply.github.com>
2021-12-04 18:09:03 +0100

    Update main.py

--- D01/main.py
+++ D01/main.py
@@ -5,9 +5,13 @@ with open("input.txt") as f:
 #Convert it to a list of ints
 lines = list(map(int, lines))
 
+three_m = []
+for i in range(len(lines)-2):
+    three_m.append(lines[i]+lines[i+1]+lines[i+2])
+
 count=0
-for i in range(1, len(lines)):
-    if lines[i-1] < lines[i]:
+for i in range(1, len(three_m)):
+    if three_m[i-1] < three_m[i]:
         count+=1
 
 print(count)
