02ad9bef1555791b3b30ef8292994cbdf3308cc4
JComyn <64434554+JComyn@users.noreply.github.com>
2021-12-06 17:59:28 +0100

    Create part2.py

---
+++ D05/part2.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+with open("input.txt") as f:
+    lines = f.readlines()
+
+diagram = np.zeros((1000,1000),dtype='i')
+
+for line in lines:
+
+    line = line.split("->")
+    # x1,y1 -> x2,y2
+    x1 = int(line[0].split(",")[0])
+    y1 = int(line[0].split(",")[1])
+    x2 = int(line[1].split(",")[0])
+    y2 = int(line[1].split(",")[1])
+
+    if x1 == x2:
+        n = abs(y2-y1)
+        for i in range(n+1):
+            if y1 < y2:
+                diagram[y1+i][x1] += 1
+            else:
+                diagram[y1-i][x1] += 1
+        continue
+
+    if y1 == y2:
+        n = abs(x2-x1)
+        for i in range(n+1):
+            if x1 < x2:
+                diagram[y1][x1+i] += 1
+            else:
+                diagram[y1][x1-i] += 1
+        continue
+
+    if x1 == y2 and y1 == x2:
+        n = abs(x1-y1)
+        for i in range(n+1):
+            if x1 > x2:
+                diagram[y1+i][x1-i] += 1
+            else:
+                diagram[y1-i][x1+i] += 1
+        continue
+
+    if abs(x1-x2) == abs(y1-y2):
+        n = abs(x1-x2)
+        for i in range(n+1):
+            if x1 > x2 and y1 > y2:
+                diagram[y1-i][x1-i] += 1
+            elif x1 < x2 and y1 < y2:
+                diagram[y1+i][x1+i] += 1
+            elif x1 < x2 and y1 > y2:
+                diagram[y1-i][x1+i] += 1
+            elif x1 > x2 and y1 < y2:
+                diagram[y1+i][x1-i] += 1
+        continue
+
+#Count elements of the diagram >= 2
+count = 0
+for x in diagram:
+    for y in x:
+        if y >= 2:
+            count += 1
+
+print(diagram)
+print(count)
